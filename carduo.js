e648d35bb8464fffa0c9000455e5f2c7864b9d1e
Soronie <roarroyo@ucdavis.edu>
2017-12-05 23:40:33 -0800

    random colorizer bug fixed; layout shift; just need to refactor everything next

--- carduo.js
+++ carduo.js
@@ -1,24 +1,27 @@
-// Select all the cards
-var cards = document.querySelectorAll(".fa:not(.lives)");
+var cards = document.querySelectorAll(".card");
+// Select all the symbols of cards
+var symbols = document.querySelectorAll(".fa:not(.lives)");
+var types = [
+	"fire",
+	"gamepad",
+	"shield",
+	"chain-broken",
+	"leaf",
+	"music",
+	"smile-o",
+	"heart"
+];
 // Keep track of colors for each set of icons
 var cardColors = {};
 var first = null;
 var second = null;
-var gameRunning = true;
+var gameRunning = false;
+var numberOfCards = 16;
 
 var scoreDisplay = document.querySelector("#score");
 var livesDisplay = document.querySelector("#lives");
 var initialLives = Number(livesDisplay.textContent);
-
-// Assign colors based on icon
-cards.forEach(function(card){
-	var key = card.classList[1];
-	// If the current icon is new, store a random color for it
-	if(!cardColors[key])
-		cardColors[key] = randomColor();
-	// Assign the card's color based on the key
-	card.style.backgroundColor = cardColors[key];
-});
+var randomPositions;
 
 // When a card is clicked, mark it as selected
 // and/or check if the first and second selected cards match
@@ -31,11 +34,13 @@ $(".fa:not(.lives)").on("click", function(){
 	{
 		first = icon;
 		first.addClass("selected");
+		first.animate({ opacity: 1}, 200);
 	}
 	else
 	{
 		second = icon;
 		second.addClass("selected");
+		second.animate({ opacity: 1}, 200);
 
 		var firstIcon = first.attr("class").split(' ')[1];
 		var secondIcon = second.attr("class").split(' ')[1];
@@ -43,8 +48,8 @@ $(".fa:not(.lives)").on("click", function(){
 		if(firstIcon === secondIcon)
 		{	// If the icons match, make them invisible
 			// Make them fade then set opacity to 0
-			$(first).parent().fadeTo(250, 0);
-			$(second).parent().fadeTo(250, 0);
+			$(first).parent().fadeTo(750, 0);
+			$(second).parent().fadeTo(750, 0);
 		}
 		else
 		{	// If the icons don't match, deduct lives
@@ -53,22 +58,35 @@ $(".fa:not(.lives)").on("click", function(){
 			second.removeClass("selected");
 
 			livesDisplay.textContent = Number(livesDisplay.textContent)-1;
+
+			second.animate({ opacity: 0}, 400);
+			first.animate({ opacity: 0}, 400);
 		}
 
 		// Discard current cards for the next round
 		first = second = null;
 	}
 
-	// Increment the score if th round is completed
+	// Increment the score if the round is completed
 	// End the game once there are no remaining lives
 	if($(".selected").length === $(".card").length)
 	{
 		scoreDisplay.textContent = Number(scoreDisplay.textContent)+1;
-		livesDisplay.textContent = initialLives;
+		setTimeout(revealCards, 1250);
 	}
 	else if(Number(livesDisplay.textContent) === 0)
+	{
 		gameRunning = false;
+		$(".btn").text("Play Again?");
+		$(".btn").css("visibility", "visible");
+	}
 
+});
+
+$(".btn").on("click", function(){
+	$(this).css("visibility", "hidden");
+	scoreDisplay.textContent = 0;
+	revealCards();
 });
 
 // Return a random color of somewhat strong intensity
@@ -77,4 +95,53 @@ function randomColor() {
 	for(var i = 0; i < 3; i++)
 		intensities.push(Math.floor(Math.random()*256 - 64));
 	return "rgb(" + intensities[0] + ", " + intensities[1] + ", " + intensities[2] + ")";
+}
+
+function generateRandomPositions(num){
+	var arr = [];
+
+	while(arr.length < num){
+		var randomNumber = Math.floor(Math.random()*numberOfCards);
+		if(arr.indexOf(randomNumber) !== -1) continue;
+		arr[arr.length] = randomNumber;
+	}
+
+	return arr;
+}
+
+function revealCards() {
+	var newCards = $(".fa:not(.lives)");
+	cardColors = {};
+
+	livesDisplay.textContent = initialLives;
+	if(randomPositions)
+	{
+		for(var i = 0; i < symbols.length; i++)
+			symbols[randomPositions[i]].classList.remove("fa-" + types[i % types.length]);
+	}
+
+	randomPositions = generateRandomPositions(numberOfCards);
+
+	for(var i = 0; i < symbols.length; i++)
+	{
+		symbols[randomPositions[i]].classList.add("fa-" + types[i % types.length]);
+	}
+
+	$(newCards).removeClass("selected");
+
+	// Assign colors based on icon
+	symbols.forEach(function(card){
+		var key = card.classList[1];
+		// If the current icon is new, store a random color for it
+		if(!cardColors[key])
+			cardColors[key] = randomColor();
+		// Assign the card's color based on the key
+		card.style.backgroundColor = cardColors[key];
+	});
+
+	$(newCards).parent().fadeTo(750, 1);
+	$(newCards).animate({ opacity: 1 }, 750);
+	$(newCards).animate({ opacity: 0 }, 5000, function(){
+		gameRunning = true;
+	});
 }
